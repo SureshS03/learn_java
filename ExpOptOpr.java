2eed79f10972f86adadc075f8c9b04c7ed94a607
SureshS03 <suthamani51@gmail.com>
2026-02-04 13:33:05 +0530

    done simply uk already

---
+++ ExpOptOpr.java
@@ -0,0 +1,9 @@
+public class ExpOptOpr {
+    public static void main(String[] args) {
+        // + - * / %
+        double frds = 10;
+        //frds =  frds / 3; //3 not 3.333... beacuse its int, use CATSING 
+        frds = (double) frds / 3;
+        System.out.println(frds);        
+    }
+}
