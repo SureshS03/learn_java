29b90fefffa7c70b1f68251ca5175033eeea6251
SureshS03 <suthamani51@gmail.com>
2026-02-11 22:24:18 +0530

    done, seems not like important

--- Polymorphsim.java
+++ Polymorphsim.java
@@ -1,5 +1,47 @@
 public class Polymorphsim {
   public static void main(String[] args) {
-    System.out.println("Tommoworw"); 
+    // Polymorphsim is give the power to an object be as more then one type
+    
+    Car c = new Car();
+    Bike b = new Bike();
+    Scooty s = new Scooty();
+
+    // if we want to store all these object in array means we cant
+    // Car[] racers = {c,b,s} // error beacuse b and s are not Car classes
+
+    // but we can use Vechicle to store all
+    Vechicle[] racers = {c,b,s};
+    // the above array is the polymorphsim because we did make a object act as more then one
+
+    for (Vechicle x : racers) {
+      x.go();
+    }
+  }
+}
+
+class Vechicle {
+    public void go(){
+
+    }
+}
+
+class Car extends Vechicle{
+  @Override
+  public void go() {
+    System.out.println("car is going..");
+  }
+}
+
+class Bike extends Vechicle{
+  @Override
+  public void go() {
+    System.out.println("bike is going..");
+  }
+}
+
+class Scooty extends Vechicle{
+  @Override
+  public void go() {
+    System.out.println("scooty is going..");
   }
 }
