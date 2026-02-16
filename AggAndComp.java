4eb361a1c89e35d3207b45e475c972447c06ad45
SureshS03 <suthamani51@gmail.com>
2026-02-16 19:21:19 +0530

    notes only

---
+++ AggAndComp.java
@@ -0,0 +1,13 @@
+public class AggAndComp {
+    public static void main(String[] args) {
+        
+        // aggregation is a concept that, relationship between object that, who has who?
+        // like book can be indepeneded but libary must have books so libary have books as its in structure
+        // and libary can access the books and its methods bcz it have in its structure
+
+        // composition is same as aggregation with some different that is 
+        // here can we build big complex object by using smaller ones
+        // like car object have enginee object and enginee must be created while creating car
+
+    }
+}
