7f9f2cd323d4b16df767f8d70cbc38b09722cae1
SureshS03 <suthamani51@gmail.com>
2026-02-16 19:21:10 +0530

    done, ezz

---
+++ Ternary.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Ternary {
+    public static void main(String[] args) {
+        
+        // Ternary is a small if else statement
+
+        Scanner sc = new Scanner(System.in);
+
+        System.err.println("Enter the num");
+        int x = sc.nextInt();
+        System.err.println(evenOrOdd(x));
+        
+
+    }
+
+    static String evenOrOdd(int x) {
+        return (x % 2 == 0) ? "even" : "odd"; // check the statment and work like dart
+    }
+
+}   
